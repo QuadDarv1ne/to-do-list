05f59c361f85ff11ca871b07a3281baf1bb22d18
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-08 11:54:27 +0300

    new file:   src/Controller/ProfileController.php 	new file:   templates/profile/edit.html.twig 	new file:   templates/profile/show.html.twig

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/change-password', name: 'app_profile_change_password', methods: ['GET', 'POST'])]
+    public function changePassword(
+        Request $request, 
+        UserPasswordHasherInterface $passwordHasher, 
+        EntityManagerInterface $entityManager
+    ): Response {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            // Check current password
+            if (!$passwordHasher->isPasswordValid($user, $data['currentPassword'])) {
+                $this->addFlash('error', 'Текущий пароль введен неправильно');
+                return $this->redirectToRoute('app_profile_change_password');
+            }
+            
+            // Set new password
+            $newPassword = $data['newPassword'];
+            $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+            
+            // Update password changed date
+            $user->setPasswordChangedAt(new \DateTime());
+            
+            $entityManager->flush();
+            
+            $this->addFlash('success', 'Пароль успешно изменен');
+            
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('profile/change_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    #[Route('/', name: 'app_profile_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        $user = $this->getUser();
+        
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+    
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $originalEmail = $user->getEmail();
+        
+        $form = $this->createForm(\App\Form\UserType::class, $user, ['is_new' => false]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Check if email is being changed and if it's already taken by another user
+            $newEmail = $user->getEmail();
+            if ($originalEmail !== $newEmail) {
+                $existingUser = $entityManager->getRepository(\App\Entity\User::class)
+                    ->findOneBy(['email' => $newEmail]);
+                
+                if ($existingUser && $existingUser->getId() !== $user->getId()) {
+                    $this->addFlash('error', 'Пользователь с таким email уже существует');
+                    return $this->redirectToRoute('app_profile_edit');
+                }
+            }
+            
+            $entityManager->flush();
+            
+            $this->addFlash('success', 'Профиль успешно обновлен');
+            
+            return $this->redirectToRoute('app_profile_show');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+}
