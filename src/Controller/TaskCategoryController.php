18e393ad53d3ee5b60f3e79ea39ca6351d266053
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-08 15:22:17 +0300

    modified:   src/Controller/TaskCategoryController.php

--- src/Controller/TaskCategoryController.php
+++ src/Controller/TaskCategoryController.php
@@ -10,16 +10,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/task-categories')]
+#[Route('/task-category')]
+#[IsGranted('ROLE_USER')]
 class TaskCategoryController extends AbstractController
 {
     #[Route('/', name: 'app_task_category_index', methods: ['GET'])]
     public function index(TaskCategoryRepository $taskCategoryRepository): Response
     {
-        $user = $this->getUser();
-        $categories = $taskCategoryRepository->findByOwner($user);
-
+        $categories = $taskCategoryRepository->findByUser($this->getUser());
+        
         return $this->render('task_category/index.html.twig', [
             'categories' => $categories,
         ]);
@@ -29,7 +30,7 @@ class TaskCategoryController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $taskCategory = new TaskCategory();
-        $taskCategory->setOwner($this->getUser());
+        $taskCategory->setUser($this->getUser());
         
         $form = $this->createForm(TaskCategoryType::class, $taskCategory);
         $form->handleRequest($request);
@@ -38,56 +39,67 @@ class TaskCategoryController extends AbstractController
             $entityManager->persist($taskCategory);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Категория успешно создана!');
+            $this->addFlash('success', 'Категория успешно создана');
 
             return $this->redirectToRoute('app_task_category_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('task_category/form.html.twig', [
-            'category' => $taskCategory,
+            'task_category' => $taskCategory,
             'form' => $form,
+            'title' => 'Создать категорию',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_task_category_show', methods: ['GET'])]
+    public function show(TaskCategory $taskCategory): Response
+    {
+        $this->denyAccessUnlessGranted('TASK_CATEGORY_VIEW', $taskCategory);
+        
+        return $this->render('task_category/show.html.twig', [
+            'task_category' => $taskCategory,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_task_category_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, TaskCategory $taskCategory, EntityManagerInterface $entityManager): Response
     {
-        // Check if user owns this category
-        if ($taskCategory->getOwner() !== $this->getUser()) {
-            throw $this->createAccessDeniedException('У вас нет прав для редактирования этой категории.');
-        }
-
+        $this->denyAccessUnlessGranted('TASK_CATEGORY_EDIT', $taskCategory);
+        
         $form = $this->createForm(TaskCategoryType::class, $taskCategory);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $taskCategory->setUpdatedAt();
             $entityManager->flush();
 
-            $this->addFlash('success', 'Категория успешно обновлена!');
+            $this->addFlash('success', 'Категория успешно обновлена');
 
             return $this->redirectToRoute('app_task_category_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('task_category/form.html.twig', [
-            'category' => $taskCategory,
+            'task_category' => $taskCategory,
             'form' => $form,
+            'title' => 'Редактировать категорию',
         ]);
     }
 
     #[Route('/{id}', name: 'app_task_category_delete', methods: ['POST'])]
     public function delete(Request $request, TaskCategory $taskCategory, EntityManagerInterface $entityManager): Response
     {
-        // Check if user owns this category
-        if ($taskCategory->getOwner() !== $this->getUser()) {
-            throw $this->createAccessDeniedException('У вас нет прав для удаления этой категории.');
-        }
-
+        $this->denyAccessUnlessGranted('TASK_CATEGORY_DELETE', $taskCategory);
+        
         if ($this->isCsrfTokenValid('delete'.$taskCategory->getId(), $request->request->get('_token'))) {
+            // Check if category has tasks
+            if (count($taskCategory->getTasks()) > 0) {
+                $this->addFlash('error', 'Нельзя удалить категорию, содержащую задачи');
+                return $this->redirectToRoute('app_task_category_index', [], Response::HTTP_SEE_OTHER);
+            }
+            
             $entityManager->remove($taskCategory);
             $entityManager->flush();
-
-            $this->addFlash('success', 'Категория успешно удалена!');
+            
+            $this->addFlash('success', 'Категория успешно удалена');
         }
 
         return $this->redirectToRoute('app_task_category_index', [], Response::HTTP_SEE_OTHER);
