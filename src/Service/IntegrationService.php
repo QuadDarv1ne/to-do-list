1fd0467766b4df541589f6d234fe23320c6cebaf
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-17 22:34:21 +0300

    modified:   src/Service/IntegrationService.php 	new file:   src/Service/MobileAPIService.php

---
+++ src/Service/IntegrationService.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Task;
+use App\Entity\User;
+
+class IntegrationService
+{
+    /**
+     * GitHub Integration
+     */
+    public function connectGitHub(User $user, string $token): array
+    {
+        // TODO: Save token to database
+        return [
+            'connected' => true,
+            'username' => 'github_user',
+            'repositories' => []
+        ];
+    }
+
+    /**
+     * Create GitHub issue from task
+     */
+    public function createGitHubIssue(Task $task, string $repo): array
+    {
+        // TODO: Call GitHub API
+        return [
+            'issue_number' => 123,
+            'url' => "https://github.com/user/$repo/issues/123",
+            'created_at' => new \DateTime()
+        ];
+    }
+
+    /**
+     * Sync GitHub issues
+     */
+    public function syncGitHubIssues(User $user, string $repo): int
+    {
+        // TODO: Fetch issues from GitHub and create tasks
+        return 0;
+    }
+
+    /**
+     * Slack Integration
+     */
+    public function connectSlack(User $user, string $webhookUrl): array
+    {
+        // TODO: Save webhook to database
+        return [
+            'connected' => true,
+            'channel' => '#general',
+            'webhook_url' => $webhookUrl
+        ];
+    }
+
+    /**
+     * Send Slack notification
+     */
+    public function sendSlackNotification(string $channel, string $message): bool
+    {
+        // TODO: Send to Slack webhook
+        return true;
+    }
+
+    /**
+     * Create task from Slack command
+     */
+    public function createTaskFromSlack(array $slackData): Task
+    {
+        // TODO: Parse Slack command and create task
+        return new Task();
+    }
+
+    /**
+     * Jira Integration
+     */
+    public function connectJira(User $user, string $domain, string $email, string $apiToken): array
+    {
+        // TODO: Save credentials to database
+        return [
+            'connected' => true,
+            'domain' => $domain,
+            'projects' => []
+        ];
+    }
+
+    /**
+     * Import Jira issues
+     */
+    public function importJiraIssues(User $user, string $projectKey): int
+    {
+        // TODO: Fetch from Jira API
+        return 0;
+    }
+
+    /**
+     * Export to Jira
+     */
+    public function exportToJira(Task $task, string $projectKey): array
+    {
+        // TODO: Create Jira issue
+        return [
+            'issue_key' => 'PROJ-123',
+            'url' => 'https://company.atlassian.net/browse/PROJ-123'
+        ];
+    }
+
+    /**
+     * Trello Integration
+     */
+    public function connectTrello(User $user, string $apiKey, string $token): array
+    {
+        // TODO: Save credentials
+        return [
+            'connected' => true,
+            'boards' => []
+        ];
+    }
+
+    /**
+     * Import Trello cards
+     */
+    public function importTrelloCards(User $user, string $boardId): int
+    {
+        // TODO: Fetch from Trello API
+        return 0;
+    }
+
+    /**
+     * Google Calendar Integration
+     */
+    public function connectGoogleCalendar(User $user, string $accessToken): array
+    {
+        // TODO: Save token
+        return [
+            'connected' => true,
+            'calendars' => []
+        ];
+    }
+
+    /**
+     * Sync tasks to Google Calendar
+     */
+    public function syncToGoogleCalendar(User $user): int
+    {
+        // TODO: Create calendar events for tasks with deadlines
+        return 0;
+    }
+
+    /**
+     * Telegram Integration
+     */
+    public function connectTelegram(User $user, string $chatId): array
+    {
+        // TODO: Save chat ID
+        return [
+            'connected' => true,
+            'chat_id' => $chatId
+        ];
+    }
+
+    /**
+     * Send Telegram message
+     */
+    public function sendTelegramMessage(string $chatId, string $message): bool
+    {
+        // TODO: Send via Telegram Bot API
+        return true;
+    }
+
+    /**
+     * Email Integration
+     */
+    public function createTaskFromEmail(array $emailData): Task
+    {
+        // TODO: Parse email and create task
+        return new Task();
+    }
+
+    /**
+     * Zapier Webhook
+     */
+    public function handleZapierWebhook(array $data): array
+    {
+        // TODO: Process Zapier webhook
+        return [
+            'success' => true,
+            'task_id' => null
+        ];
+    }
+
+    /**
+     * Get all integrations
+     */
+    public function getAvailableIntegrations(): array
+    {
+        return [
+            'github' => [
+                'name' => 'GitHub',
+                'description' => 'Синхронизация с GitHub Issues',
+                'icon' => 'fab fa-github',
+                'features' => [
+                    'Создание issues из задач',
+                    'Импорт issues в задачи',
+                    'Двусторонняя синхронизация'
+                ]
+            ],
+            'slack' => [
+                'name' => 'Slack',
+                'description' => 'Уведомления и команды в Slack',
+                'icon' => 'fab fa-slack',
+                'features' => [
+                    'Уведомления в каналы',
+                    'Создание задач из Slack',
+                    'Slash команды'
+                ]
+            ],
+            'jira' => [
+                'name' => 'Jira',
+                'description' => 'Интеграция с Atlassian Jira',
+                'icon' => 'fab fa-jira',
+                'features' => [
+                    'Импорт issues',
+                    'Экспорт задач',
+                    'Синхронизация статусов'
+                ]
+            ],
+            'trello' => [
+                'name' => 'Trello',
+                'description' => 'Импорт карточек из Trello',
+                'icon' => 'fab fa-trello',
+                'features' => [
+                    'Импорт досок',
+                    'Импорт карточек',
+                    'Сохранение структуры'
+                ]
+            ],
+            'google_calendar' => [
+                'name' => 'Google Calendar',
+                'description' => 'Синхронизация с календарем',
+                'icon' => 'fab fa-google',
+                'features' => [
+                    'Экспорт дедлайнов',
+                    'Напоминания',
+                    'Двусторонняя синхронизация'
+                ]
+            ],
+            'telegram' => [
+                'name' => 'Telegram',
+                'description' => 'Уведомления в Telegram',
+                'icon' => 'fab fa-telegram',
+                'features' => [
+                    'Мгновенные уведомления',
+                    'Создание задач из чата',
+                    'Бот команды'
+                ]
+            ],
+            'zapier' => [
+                'name' => 'Zapier',
+                'description' => 'Подключение 3000+ приложений',
+                'icon' => 'fa-bolt',
+                'features' => [
+                    'Автоматизация',
+                    'Триггеры и действия',
+                    'Неограниченные возможности'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Get user integrations
+     */
+    public function getUserIntegrations(User $user): array
+    {
+        // TODO: Get from database
+        return [];
+    }
+
+    /**
+     * Disconnect integration
+     */
+    public function disconnectIntegration(User $user, string $integration): bool
+    {
+        // TODO: Remove from database
+        return true;
+    }
+
+    /**
+     * Test integration connection
+     */
+    public function testConnection(string $integration, array $credentials): array
+    {
+        // TODO: Test API connection
+        return [
+            'success' => true,
+            'message' => 'Подключение успешно'
+        ];
+    }
+
+    /**
+     * Get integration statistics
+     */
+    public function getIntegrationStats(User $user): array
+    {
+        return [
+            'total_synced' => 0,
+            'last_sync' => null,
+            'active_integrations' => 0,
+            'sync_errors' => 0
+        ];
+    }
+}
