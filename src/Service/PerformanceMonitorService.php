8dc893f92a819aeed92155acb7f18549a2b437b0
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-17 20:03:28 +0300

    deleted:    src/Form/TaskShareType.php 	deleted:    src/Service/AdvancedErrorHandlingService.php 	deleted:    src/Service/AdvancedSecurityService.php 	new file:   src/Service/PerformanceMonitorService.php 	deleted:    src/Service/SecurityManagementService.php 	deleted:    src/Service/UserActivityService.php

---
+++ src/Service/PerformanceMonitorService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Alias for PerformanceMonitoringService for backward compatibility
+ * @deprecated Use PerformanceMonitoringService instead
+ */
+class PerformanceMonitorService extends PerformanceMonitoringService
+{
+    // This class extends PerformanceMonitoringService to maintain backward compatibility
+    // All functionality is inherited from the parent class
+}
