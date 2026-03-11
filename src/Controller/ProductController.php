0521dbc4693f42393c77a00e2923cb93b5b33f91
QuadDarv1ne <maksimqwe42@mail.ru>
2026-03-11 16:30:32 +0300

    "fix-product-controller-request-import

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
